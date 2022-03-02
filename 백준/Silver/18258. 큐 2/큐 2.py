0148c40a48bde56f2fbd6b93db964fe570b56b84
enyd97 <77716120+enyd97@users.noreply.github.com>
2022-03-03 01:03:46 +0900

    [Silver IV] Title: 큐 2, Time: 1380 ms, Memory: 239988 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,48 @@
+from collections import deque
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+if __name__ == '__main__':
+    queue = deque([])
+    N = int(input())
+
+    for _ in range(N):
+        current = input().split()
+        command = current[0]
+
+        if command == 'push':
+            queue.append(current[1])
+
+        elif command == 'pop':
+            if queue:
+                print(queue.popleft())
+            else:
+                print(-1)
+
+        elif command == 'size':
+            print(len(queue))
+
+        elif command == 'empty':
+            if queue:
+                print(0)
+            else:
+                print(1)
+
+        elif command == 'front':
+            if queue:
+                print(queue[0])
+            else:
+                print(-1)
+
+        elif command == 'back':
+            if queue:
+                print(queue[-1])
+            else:
+                print(-1)
+
+        else:
+            pass
