af6d984f32ed934c21fa0d17f79fb253250268e5
enyd97 <77716120+enyd97@users.noreply.github.com>
2024-03-08 22:52:55 +0900

    [Silver IV] Title: 카드2, Time: 144 ms, Memory: 50916 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,19 +1,23 @@
-from collections import deque
 import sys
-
+from collections import deque
 
 def input():
     return sys.stdin.readline().rstrip()
 
 
-if __name__ == '__main__':
-    queue = deque([])
-    N = int(input())
+def solution(N):
+    q = deque()
     for i in range(1, N+1):
-        queue.append(i)
-
-    while len(queue) > 1:
-        queue.popleft()
-        queue.append(queue.popleft())
-
-    print(queue[0])
+        q.append(i)
+    if len(q)==1:
+        return q[0]
+    q.popleft()
+    while len(q) >1:    
+        temp = q[0]
+        q.popleft()
+        q.append(temp)
+        q.popleft()
+    return q[0]
+    
+num = int(input())
+print(solution(num))
