7813d9a7a3f0fa3f0d2c3735e9ee5d0e8c5058c0
enyd97 <77716120+enyd97@users.noreply.github.com>
2024-04-16 00:05:22 +0900

    [Silver V] Title: 영화감독 숌, Time: 644 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,18 +1,17 @@
 import sys
-from collections import deque
-
-
 def input():
     return sys.stdin.readline().rstrip()
 
+def solution(k):
+    temp = 0
+    result = 0
+    while result < k:
+        temp = temp + 1
+        temp_str = str(temp)
+        if '666' in temp_str:
+            result = result + 1
+            
+    return temp
 
-if __name__ == '__main__':
-    N = int(input())
-    count = 0
-    num = 0
-    while count < N:
-        num += 1
-        if '666' in str(num):
-            count += 1
-
-    print(num)
+N = int(input())
+print(solution(N))
