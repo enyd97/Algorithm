e707dcafedd1dcb67748beb3c280d1e0e1c5192f
enyd97 <77716120+enyd97@users.noreply.github.com>
2024-04-17 23:39:33 +0900

    [Silver III] Title: 퇴사, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,24 +1,18 @@
 import sys
-from collections import deque
-import heapq
 
 def input():
     return sys.stdin.readline().rstrip()
 
 def solution(K):
-    #print('check day ', K)
     if K > N:
-        #print('today is bigger than N pass')
         return 0
     if (K + schedule[K][0]) > N + 1:
-        #print('today''s job is too long')
         return solution(K+1)
     return max(schedule[K][1] + solution(K + schedule[K][0]), solution(K+1))
 
-
 N = int(input())
 schedule = []
-schedule.append([0,0]) # dummy value for index 0
+schedule.append([0,0])
 for _ in range(N):
     a, b = map(int, input().split())
     temp = []
