9ff985f644df971b6b350aa39ca86cd5f117f736
enyd97 <77716120+enyd97@users.noreply.github.com>
2024-04-17 23:38:36 +0900

    [Silver III] Title: 퇴사, Time: 76 ms, Memory: 35684 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,34 +1,29 @@
 import sys
 from collections import deque
-
+import heapq
 
 def input():
     return sys.stdin.readline().rstrip()
 
-
-def func(date, remain, money):
-    global N
-    # exit
-    if date >= N:
-        if remain > 0:
-            return 0
-        return money
-
-    result = func(date + 1, remain - 1, money)
-    if remain > 0:
-        return result
-    return max(result, func(date + 1, T[date] - 1, money + P[date]))
+def solution(K):
+    #print('check day ', K)
+    if K > N:
+        #print('today is bigger than N pass')
+        return 0
+    if (K + schedule[K][0]) > N + 1:
+        #print('today''s job is too long')
+        return solution(K+1)
+    return max(schedule[K][1] + solution(K + schedule[K][0]), solution(K+1))
 
 
 N = int(input())
-
-if __name__ == '__main__':
-
-    T = []
-    P = []
-    for i in range(N):
-        a ,b = map(int, input().split())
-        T.append(a)
-        P.append(b)
-
-    print(func(0, 0, 0))
+schedule = []
+schedule.append([0,0]) # dummy value for index 0
+for _ in range(N):
+    a, b = map(int, input().split())
+    temp = []
+    temp.append(a)
+    temp.append(b)
+    schedule.append(temp)
+    
+print(solution(1))
